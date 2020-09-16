b54d1b560469b956b6ae571d191c854648b40006
annisadp <annisadwipus35@gmail.com>
2020-09-16 19:53:14 +0700

    first commit

---
+++ 19081010003-Annisa Dwi Puspitarini.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+	
+	printf ("Perkenalan Diri\n\n");
+	printf ("Nama : Annisa Dwi Puspitarini\n");
+	printf ("NPM : 19081010003\n");
+	printf ("Kelas : Pemrogrograman Lanjut C\n");
+	printf ("Semester : 3\n");
+	printf ("Alamat : Kavling Pepe Indah, Sedati-Sidoarjo\n");
+	
+	return 0;
+}
