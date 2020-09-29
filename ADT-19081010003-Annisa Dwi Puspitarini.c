548b6b4e7b0f44ac6d04e74d574e684203bd5339
annisadp <annisadwipus35@gmail.com>
2020-09-29 11:37:40 +0700

    second commit

---
+++ ADT-19081010003-Annisa Dwi Puspitarini.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdbool.h>
+ 
+struct Mahasiswa {
+   char  nama[50];
+   char  kuliah[50];
+   unsigned int  NPM;
+   char  StatusMahasiswa[50];
+  };
+ 
+int main(void)
+{
+  struct Mahasiswa Mahasiswa01; 
+ 
+  printf("***** Data Diri Mahasiswa *****\n");
+  printf("\n");
+  
+  printf("Nama: ");
+  gets(Mahasiswa01.nama);
+ 
+  printf("Kuliah: ");
+  gets(Mahasiswa01.kuliah);
+ 
+  printf("StatusMahasiswa: ");
+  gets(Mahasiswa01.StatusMahasiswa);
+ 
+  printf("NPM: ");
+  scanf("%i",&Mahasiswa01.NPM);
+  
+  printf("Hasil: \n");
+ 
+  printf("%s kuliah di %s ", Mahasiswa01.nama, Mahasiswa01.kuliah);
+  printf("dengan NPM %i yang berstatus %s\n", Mahasiswa01.NPM, Mahasiswa01.StatusMahasiswa);
+ 
+ 
+ 
+  return 0;
+}	
